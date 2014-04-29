7e210eef38c38c70817411560f5551f3d242dc39
juliograves <julio.cesar.graves@gmail.com>
2014-04-28 23:36:25 -0300

    Update cachematrix.R
    Peer assignment program added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x=numeric(),lines,cols){
+    m <- matrix(x,lines,cols)
+    inverse <-NULL
+    set <- function(x,l,c) {
+        m <<- matrix(x,l,c)
+        inverse <<- NULL
+    }
+    
+    get <- function() {
+        m
+    }
+    
+    setinverse <- function(inver){
+        inverse <<- inver
+    }
+    
+    getinverse <- function(){
+        inverse
+    }
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(objMakeCache,...){
+    matinv <- objMakeCache$getinverse()
+    if(!is.null(matinv)){
+        message("getting cached inverse")
+        return(matinv)
+    }
+    objMakeCache$setinverse(solve(objMakeCache$get()))
+    objMakeCache$getinverse()
 }
+    
+    
+    
+## YOU CAN TEST IT BY USING
+    
+    
+a <- makeCacheMatrix(sample(1:10),2,5)
+a$get()    
+a$set(sample(1:25),5,5) 
+a$get()    
+
+cacheSolve(a)
+a$getinverse()    
